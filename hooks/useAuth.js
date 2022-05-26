8e5722343db9ee5f835da2d5d57d2941a6290d46
Edward Umana <edwarduma@icloud.com>
2022-05-26 13:28:09 -0500

    Fix of capital letter 🔨

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,4 +1,4 @@
 import { useContext } from 'react';
-import AuthContext from '../context/AuthContext';
+import AuthContext from '../context/authContext';
 
 export default () => useContext(AuthContext);
